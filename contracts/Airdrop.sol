d6dad89b9f8f87d5c45421dfad72fbc933f87724
Sr Developer <dev.l33@zoho.com>
2018-11-14 10:57:05 +0900

    first commit

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,99 @@
+pragma solidity ^0.4.18;
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public view returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+pragma solidity ^0.4.18;
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+
+/**
+ * @title Airdrop
+ * @dev Token Batch Transfer for Airdrop campaign
+ */
+contract Airdrop is Ownable {
+
+  ERC20 token;
+
+  /**
+  * Contructor
+  */
+  constructor(address _token) public {
+    require(_token != address(0));
+    token = ERC20(_token);
+  }
+
+  /* Batch token transfer. Used by contract creator to distribute initial tokens to holders */
+  function batchTransfer(address[] _recipients, uint[] _values) external onlyOwner {
+    require(_recipients.length > 0 && _recipients.length == _values.length);
+
+    for (uint i = 0; i < _recipients.length; i++) {
+      token.transfer(_recipients[i], _values[i] * 1 ether);
+    }
+  }
+
+  function withdraw(address recipient, uint _value) external onlyOwner {
+    token.transfer(recipient, _value);
+  }
+
+}
